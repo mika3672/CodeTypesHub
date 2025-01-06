61b34b00f5d2effa2d0b39849cd0e5875d90f86f
mattnix6 <nirina.ran@smartone.ai>
2025-01-06 16:37:41 +0300

    shopify: add get_size_labels function

---
+++ Exemples de Code/Shopify/get_size_labels.js
@@ -0,0 +1,4 @@
+(function (data_attribute_stringified) {
+    let sizeLabels = window.productInfo?.label || [];
+    return sizeLabels.join('<!LIST!>');
+})('DATA_ATTRIBUTE_INSTANCE_LIVE_SCRAPPER_SPOT');
