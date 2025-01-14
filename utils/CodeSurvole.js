26ffac98b265f835dcb6e0870f9b26442fff62e8
FlorenceMam <florence.mam@smartone.ai>
2025-01-14 11:39:01 +0300

    update codeSurvole

--- utils/CodeSurvole.js
+++ utils/CodeSurvole.js
@@ -1,5 +1,5 @@
-/** 
- * Fonction pour simuler une survole par Florence
+/**
+ * Fonction pour simuler une survol par Florence
  *
  * ------------------------Consigne --------------------------
  * Declarer le selecteur de categorie , faire une parcour sur chaque elements
@@ -11,22 +11,25 @@
 (function (data_attribute_stringified) {
     let hrefAttributes = [];
     let elements = document.evaluate(
-        '//li[contains(text(),"Linge")]//..//li[not(contains(text(),"Chemin")) and not(contains(text(),"cadeaux")) and not(contains(text(),"Collection"))]',
+        `//li[contains(text(),"Linge")]//..//li[not(contains(text(),"Chemin")) and not(contains(text(),"cadeaux")) and not(contains(text(),"Collection"))]`,
         document,
         null,
         XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
         null
     );
+
     for (let i = 0; i < elements.snapshotLength; i++) {
         let el = elements.snapshotItem(i);
-        let hoverEvent = new MouseEvent('mouseover', { 
+        let hoverEvent = new MouseEvent(`mouseover`, { 
             bubbles: true
         });
         el.dispatchEvent(hoverEvent);  
-        let select = document.querySelectorAll('ul li[class="nav-link"] a:not([href*="decouvrir"])');
+        
+        let select = document.querySelectorAll(`ul li[class="nav-link"] a:not([href*="decouvrir"])`);
         for (let i = 0; i < select.length; i++) {
-            hrefAttributes.push(select[i].getAttribute("href"));
+            hrefAttributes.push(select[i].getAttribute(`href`));
         }
     }
-    return hrefAttributes.join('<!LIST!>')
-})('DATA_ATTRIBUTE_INSTANCE_LIVE_SCRAPPER_SPOT');
+
+    return hrefAttributes.join(`<!LIST!>`);
+})(`DATA_ATTRIBUTE_INSTANCE_LIVE_SCRAPPER_SPOT`);
